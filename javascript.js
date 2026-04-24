e026e6f7c0c435b5da17bf512f6a6aafcde12e4e
“pminh530” <250648659+pminh530-maker@users.noreply.github.com>
2026-04-24 16:36:33 +0700

    Add a reset button and dynamic styling for cells
    Earlier, I tried to set a fixed with and height for each cell, which is
    problematic when we generate a new grid.
    
    I knew that we had to use a math function to store this dynamic sizing
    value, but my attempts at including them directly in the CSS selectors
    were unsuccessful.
    
    The solution is to store the math function in a separate variable/const.
    Then, we can convert the result into a string using the binary plus in
    the CSS selector. Voila!

--- javascript.js
+++ javascript.js
@@ -1,18 +1,38 @@
+const title = document.querySelector('h1');
 const boardFrame = document.querySelector('#board-frame');
+const resetButton = document.createElement('button');
+resetButton.textContent = "RESET";
+boardFrame.insertAdjacentElement('beforebegin', resetButton);
 
-let row = 0;
-let column = 0;
+let row;
+let column;
+let gridSize = 16;
 
-function createCell(number) {
-    for(row = 0; row < 16; row ++) {
-        for(column = 0; column < 16; column++) {
+function createGrid(gridSize) {
+    for(row = 0; row < gridSize; row ++) {
+        for(column = 0; column < gridSize; column++) {
             let cell = document.createElement('div');
             cell.classList.add('cell');
-            cell.style.width = "60px";
-            cell.style.height = "60px";
+            const cellSize =  960 / gridSize;
+            cell.style.width = cellSize + 'px';
+            cell.style.height = cellSize + 'px';
+            cell.addEventListener("mouseenter", function changeCellColor() {
+                cell.style.backgroundColor = "gray";
+            })
             boardFrame.appendChild(cell)
         }
     }
 }
 
-createCell()
+createGrid(gridSize);
+
+resetButton.addEventListener('click', function createNewGrid() {
+    let gridSize = Number(prompt("How many squares do you want on each side?", "16"));
+    if (gridSize > 100) {
+        alert("100 is the limit. Please try again!");
+        return gridSize;
+    } else {
+    boardFrame.textContent = "";
+    createGrid(gridSize);
+    }
+})
