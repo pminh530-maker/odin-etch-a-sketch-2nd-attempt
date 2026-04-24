a160b55f31742e172cfc0f31cd9a0feb1826264d
“pminh530” <250648659+pminh530-maker@users.noreply.github.com>
2026-04-24 17:13:10 +0700

    Generate random cell colors and fix reset rules.
    Do not use return in the for loop when generating new cell colors, or
    the function will stop and the code breaks.
    
    Strings are an array-like object, so we can use bracket notation to get
    access to the characters in the string using their index numbers!
    
    Remove the alert message from the createNewGrid function, or it will
    create an infinite loop of alert message without letting the user enter
    a new input.

--- javascript.js
+++ javascript.js
@@ -16,8 +16,13 @@ function createGrid(gridSize) {
             const cellSize =  960 / gridSize;
             cell.style.width = cellSize + 'px';
             cell.style.height = cellSize + 'px';
-            cell.addEventListener("mouseenter", function changeCellColor() {
-                cell.style.backgroundColor = "gray";
+            cell.addEventListener("mouseenter", function generateRandomCellColor() {
+                let hexColorCharacters = '0123456789ABCDEF';
+                let newRandomCellColor = '#';
+                for(let i = 0; i < 8; i++) {
+                    newRandomCellColor += hexColorCharacters[Math.floor(Math.random() * 16)]
+                }
+                cell.style.backgroundColor = newRandomCellColor;
             })
             boardFrame.appendChild(cell)
         }
@@ -28,11 +33,11 @@ createGrid(gridSize);
 
 resetButton.addEventListener('click', function createNewGrid() {
     let gridSize = Number(prompt("How many squares do you want on each side?", "16"));
-    if (gridSize > 100) {
-        alert("100 is the limit. Please try again!");
-        return gridSize;
-    } else {
-    boardFrame.textContent = "";
-    createGrid(gridSize);
+    while (gridSize > 100) {
+        if (gridSize <= 100) {
+            boardFrame.textContent =  '';
+            createGrid(gridSize);
+        }
+        gridSize = Number(prompt("Sorry, 100 is the limit. Please try again."));
     }
 })
